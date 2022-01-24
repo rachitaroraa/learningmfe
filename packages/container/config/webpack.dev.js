33dbcadd83edb5ce1b0a37132f094cde2f78d1a6
Rachit Arora <rachitarora@Rachits-MacBook-Air.local>
2022-01-24 23:46:43 +0530

    Production configuration and CI/CD

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -1,7 +1,7 @@
 const {merge} = require('webpack-merge')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
 const commonConfig = require('./webpack.common')
+const packageJSON = require('../package.json')
 
 const devConfig = {
     mode: 'development',
@@ -16,10 +16,8 @@ const devConfig = {
             name: 'container',
             remotes: {
                 marketing: 'marketing@http://localhost:8081/remoteEntry.js'
-            }
-        }),
-        new HtmlWebpackPlugin({
-            template: './public/index.html'
+            },
+            shared: packageJSON.dependencies
         })
     ]
 }
--- packages/marketing/config/webpack.dev.js
+++ packages/marketing/config/webpack.dev.js
@@ -1,7 +1,7 @@
 const { merge } = require('webpack-merge')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
 const commonConfig = require('./webpack.common')
+const packageJSON = require('../package.json')
 
 const devConfig = {
     mode: 'development',
@@ -17,10 +17,8 @@ const devConfig = {
             filename: 'remoteEntry.js',
             exposes: {
                 './MarketingApp': './src/bootstrap'
-            }
-        }),
-        new HtmlWebpackPlugin({
-            template: './public/index.html'
+            },
+            shared: packageJSON.dependencies
         })
     ]
 }
