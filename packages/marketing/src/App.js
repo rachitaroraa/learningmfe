4c978d89b6b8adc23b600598765e7954b78f2c5f
Rachit Arora <rachitarora@Rachits-MacBook-Air.local>
2022-01-25 23:24:55 +0530

    just for testing

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -4,7 +4,7 @@ import { StylesProvider } from '@material-ui/core/styles'
 
 import Landing from './components/Landing'
 import Pricing from './components/Pricing'
-//comment from testing
+//comment from testing again
 export default () => {
     return(
         <div>
