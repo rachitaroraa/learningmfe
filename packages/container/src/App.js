3e5ac645a7bbd557bc3569f2a545bed76af549b2
Rachit Arora <rachitarora@Rachits-MacBook-Air.local>
2022-01-26 01:04:28 +0530

    Css production issue collision

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles'
 import MarketingApp from './components/MarketingApp'
 import Header from './components/Header'
 
+const generateClassName = new createGenerateClassName({
+    productionPrefix: 'co'
+})
 export default () =>
     <BrowserRouter>
-        <div>
-            <Header />
-            <MarketingApp />
-        </div>
+        <StylesProvider generateClassName={generateClassName}>
+            <div>
+                <Header />
+                <MarketingApp />
+            </div>
+        </StylesProvider>
     </BrowserRouter>
