2883335ccee57f80749e5c521fa3aa635ab42660
Rachit Arora <rachitarora@Rachits-MacBook-Air.local>
2022-01-27 01:11:16 +0530

    navigation handling in different mfes

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,15 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { createMemoryHistory, createBrowserHistory } from 'history'
 import App from './App'
 
-const mount = (el) => {
-    ReactDOM.render(<App />, el)
+const mount = (el, { onNavigate, defaultHistory }) => {
+    const history = defaultHistory || createMemoryHistory()
+    onNavigate && history.listen(onNavigate)
+    ReactDOM.render(<App history={history} />, el)
+    return {
+        onParentNavigate({ pathname: nextPathName }) {
+            
+            const pathname = history.location.pathname
+            console.log('is this working', pathname,nextPathName)
+            if (pathname !== nextPathName) {
+                history.push(nextPathName)
+            }
+        }
+    }
 }
 
-if(process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === 'development') {
     const devRoot = document.querySelector('#_marketing-dev-root')
-    if(devRoot) {
-        mount(devRoot)
+    if (devRoot) {
+        mount(devRoot, {defaultHistory: createBrowserHistory()})
     }
 }
 
